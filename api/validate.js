1b70d78a0857423e9c22defc2801faf7011f246d
wiber2065939-droid <wiber2065939@gmail.com>
2025-10-06 22:09:30 +0800

    Update validate.js

--- api/validate.js
+++ api/validate.js
@@ -183,6 +183,3 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: 'Internal server error' });
   }
 }
-    res.status(500).json({ error: 'Internal server error' });
-  }
-}
