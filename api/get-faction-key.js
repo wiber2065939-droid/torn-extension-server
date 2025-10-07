22c90fce60e4d8b3ff038815db6e774789cc6c5c
wiber2065939-droid <wiber2065939@gmail.com>
2025-10-07 21:09:21 +0800

    Update get-faction-key.js

--- api/get-faction-key.js
+++ api/get-faction-key.js
@@ -2,8 +2,13 @@
 export default async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
