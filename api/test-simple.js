e45afcb8fa266da49dafe3add5eaa929b10f1bc7
wiber2065939-droid <wiber2065939@gmail.com>
2025-10-11 17:35:39 +0800

    Delete api/test-simple.js

--- api/test-simple.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-    return res.status(200).json({
-        success: true,
-        message: 'Simple test works!',
-        timestamp: new Date().toISOString()
-    });
-}
